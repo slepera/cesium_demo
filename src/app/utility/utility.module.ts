d62d5fa139aa5d2bde16a5c682209866cfcb1f50
slepera <salvatore.lepera@next.it>
2021-07-30 15:54:11 +0200

    minor changes

--- src/app/utility/utility.module.ts
+++ src/app/utility/utility.module.ts
@@ -14,15 +14,13 @@ export class UtilityModule {
     var orientation;
     if (previous != undefined) {
       var direction = Cesium.Cartesian3.subtract(current, previous, new Cesium.Cartesian3());
-      if (Cesium.Cartesian3.distance(direction, new Cesium.Cartesian3(0, 0, 0)) != 0) {
+      var d = Cesium.Cartesian3.distance(direction, new Cesium.Cartesian3(0, 0, 0));
+      if (d != 0) {
         Cesium.Cartesian3.normalize(direction, direction);
         var rotationMatrix = Cesium.Transforms.rotationMatrixFromPositionVelocity(previous, direction);
         var rot90 = Cesium.Matrix3.fromRotationY(Cesium.Math.toRadians(p));
         Cesium.Matrix3.multiply(rotationMatrix, rot90, rotationMatrix);
         orientation = Cesium.Quaternion.fromRotationMatrix(rotationMatrix);
-      }
-      else {
-        orientation = Cesium.Transforms.headingPitchRollQuaternion(current, new Cesium.HeadingPitchRoll(0, Cesium.Math.toRadians(p), 0));
       }
     }
     return orientation;
