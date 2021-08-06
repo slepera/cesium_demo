0c89aecb22bdd5368c66f08aa6f2cfdf0810dd0e
Emanuele <dangelo.emanuele.94@gmail.com>
2021-08-06 09:55:08 +0200

    modified chart and gauge ranges

--- src/app/data-chart/data-chart.component.ts
+++ src/app/data-chart/data-chart.component.ts
@@ -38,31 +38,6 @@ export class DataChartComponent {
   public subscription;
   public myBoolean = true;
   private chartName: string;
-  private tempData: dataType = {
-    x: 0,
-    y: ''
-  };
-  private umidData: dataType = {
-    x: 0,
-    y: ''
-  };
-  private windData: dataType = {
-    x: 0,
-    y: ''
-  };
-  private waterTemperatureData: dataType = {
-    x: 0,
-    y: ''
-  };
-  private waterSalinityData: dataType = {
-    x: 0,
-    y: ''
-  };
-  private waterPhData: dataType = {
-    x: 0,
-    y: ''
-  };
-
   private selectedData: dataType = {
     x: 0,
     y: ''
@@ -70,40 +45,88 @@ export class DataChartComponent {
   public selectedType: string;
   public gaugeLabel;
   public gaugeAppendText;
+  public min = 0;
+  public max = 100;
   gaugeType: NgxGaugeType = "arch";
-  gaugeValue = 28.3;
-  thresholdConfig = {
-    '0': { color: 'green' },
-    '40': { color: 'orange' },
-    '75.5': { color: 'red' }
-  };
+  gaugeValue = 0;
+  thresholdConfig;
+  thresholdList = {
+    'air_temperature':  {
+      '0': {color: 'blue'},
+      '25': {color: 'green'},
+      '35': {color: 'yellow'},
+      '40': {color: 'red'}
+    },
+    'air_humidity': {
+      '0': {color: 'blue'},
+      '55': {color: 'green'},
+      '65': {color: 'yellow'},
+      '75.5': {color: 'red'}
+  },
+    'air_wind':  {
+      '0': { color: 'green' },
+      '12': { color: 'yellow' },
+      '17': { color: 'red' }
+    },
+    'water_temperature':  {
+      '0': {color: 'green'},
+      '17': {color: 'yellow'},
+      '20': {color: 'orange'},
+      '23': {color: 'red'}
+    },
+    'water_salinity':  {
+      '3.4': { color: 'green' },
+      '3.5': { color: 'orange' },
+      '3.6': { color: 'red' }
+    },
+    'water_ph':  {
+      '0': {color: 'red'},
+      '3': {color: 'orange'},
+      '5': {color: 'green'},
+      '9': {color: 'blue'},
+      '11': {color: 'purple'}
+    }
+  }
   bcolor = 'white';
   constructor(private dataManager: DataManagerService) {
     this.selectedType = this.dataManager.selectedData;
+    this.thresholdConfig = this.thresholdList[this.selectedType];
     if (this.selectedType == 'air_temperature') {
         this.chartName = 'Average Temperature';
         this.gaugeLabel = "Temperature";
         this.gaugeAppendText = "°C";
+        this.min = 20;
+        this.max = 40;
       } else if (this.selectedType == 'air_humidity') {
         this.chartName = 'Humidity';
         this.gaugeLabel = "Humidity";
         this.gaugeAppendText = "%";
+        this.min = 40;
+        this.max = 80;
       } else if (this.selectedType == 'air_wind') {
         this.chartName = 'Wind';
         this.gaugeLabel = "Wind speed";
         this.gaugeAppendText = "km/h";
+        this.min = 0;
+        this.max = 30;
       } else if (this.selectedType == 'water_temperature') {
         this.chartName = 'Water Temperature';
         this.gaugeLabel = "Temperature";
         this.gaugeAppendText = "°C";
+        this.min = 10;
+        this.max = 30;
       } else if (this.selectedType == 'water_salinity') {
         this.chartName = 'Water Salinity';
         this.gaugeLabel = "Salinity";
         this.gaugeAppendText = "%";
+        this.min = 3;
+        this.max = 4;
       } else if (this.selectedType == 'water_ph') {
         this.chartName = 'pH';
         this.gaugeLabel = "pH";
         this.gaugeAppendText = "";
+        this.min = 0;
+        this.max = 14;
       }
 
     this.chartOptions = {
@@ -185,44 +208,9 @@ export class DataChartComponent {
 
     this.subscription = dataManager.chartMessages.subscribe(msg => {
       n = Number(msg.x);
-      if (msg.msg_type === 'air_temperature') {
-        this.tempData.x = n;
-        this.tempData.y = msg.y;
-      } else if (msg.msg_type === 'air_humidity') {
-        this.umidData.x = n;
-        this.umidData.y = msg.y;
-      } else if (msg.msg_type === 'air_wind') {
-        this.windData.x = n;
-        this.windData.y = msg.y;
-      } else if (msg.msg_type === 'water_temperature') {
-        this.waterTemperatureData.x = n;
-        this.waterTemperatureData.y = msg.y;
-      } else if (msg.msg_type === 'water_salinity') {
-        this.waterSalinityData.x = n;
-        this.waterSalinityData.y = msg.y;
-      }  else if (msg.msg_type === 'water_ph') {
-        this.waterPhData.x = n;
-        this.waterPhData.y = msg.y;
-      } 
-
-      if (this.selectedType == 'air_temperature') {
-        this.selectedData.x = this.tempData.x;
-        this.selectedData.y = this.tempData.y;
-      } else if (this.selectedType == 'air_humidity') {
-        this.selectedData.x = this.umidData.x;
-        this.selectedData.y = this.umidData.y;
-      } else if (this.selectedType == 'air_wind') {
-        this.selectedData.x = this.windData.x;
-        this.selectedData.y = this.windData.y;
-      } else if (this.selectedType == 'water_temperature') {
-        this.selectedData.x = this.waterTemperatureData.x;
-        this.selectedData.y = this.waterTemperatureData.y;
-      } else if (this.selectedType == 'water_salinity') {
-        this.selectedData.x = this.waterSalinityData.x;
-        this.selectedData.y = this.waterSalinityData.y;
-      } else if (this.selectedType == 'water_ph') {
-        this.selectedData.x = this.waterPhData.x;
-        this.selectedData.y = this.waterPhData.y;
+      if(msg.msg_type === this.selectedType){
+        this.selectedData.x = n;
+        this.selectedData.y = msg.y;
       }
       if(this.selectedData.y != ''){
         this.chartOptions.series = [{
